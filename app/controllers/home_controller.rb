c7d67366a392dfe6e3138cf75ecd5c224794169c
mariollet <mariollet@outlook.fr>
2019-11-03 01:42:47 +0100

    final commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,6 @@ class HomeController < ApplicationController
   def welcome
     @gossips = Gossip.all
     @users = User.all
-    if City.all.length == 4
-      City.create(name: "Lyon")
-      City.create(name: "Marseille")
-    end
   end
 
 end
