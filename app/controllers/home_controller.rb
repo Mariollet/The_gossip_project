e74102ec8a6e920f5ef82104f73988b06b84e707
mariollet <mariollet@outlook.fr>
2019-11-03 01:41:44 +0100

    add city

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,9 @@ class HomeController < ApplicationController
   def welcome
     @gossips = Gossip.all
     @users = User.all
-    if City.all.length == 0
-      City.create(name: "Paris")
-      City.create(name: "Montpellier")
-      City.create(name: "Grenoble")
-      City.create(name: "Avignon")
+    if City.all.length == 4
+      City.create(name: "Lyon")
+      City.create(name: "Marseille")
     end
   end
 
